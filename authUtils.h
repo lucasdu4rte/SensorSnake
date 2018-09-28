08ea8f2b8b37e2a08b3b78c0faf741367db0ad17
Lucas Duarte <lucasduarte1112@outlook.com>
2018-09-28 13:36:46 -0300

    auth values

--- authUtils.h
+++ authUtils.h
@@ -1,7 +1,7 @@
 #ifndef AUTH_NETWORK_AND_FIREBASE
 #define AUTH_NETWORK_AND_FIREBASE
-#define FIREBASE_HOST "sensor-temp-humid.firebaseio.com"
-#define FIREBASE_AUTH "8qbkiuCSul3Y5XXIjWk1AVIcUbf9clqe6E84YIbt"
-#define WIFI_SSID "carbon"
-#define WIFI_PASSWORD "11223344l"
+#define FIREBASE_HOST "***.firebaseio.com"
+#define FIREBASE_AUTH "***"
+#define WIFI_SSID "***"
+#define WIFI_PASSWORD "***"
 #endif
